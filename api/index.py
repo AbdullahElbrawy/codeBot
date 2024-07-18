1dc92d8d3e3f5cc9759e3edf55eb98fa62c0d909
AbdullahElbarawy <abdullahalbrawy00@gmail.com>
2024-07-18 21:38:43 +0300

    main

--- api/index.py
+++ api/index.py
@@ -32,9 +32,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        with open(join(dir, '..', 'data', 'file.txt'), 'r') as file:
-          for line in file:
-            self.wfile.write(line.encode())
+       
         return
 
 
